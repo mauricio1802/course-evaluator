0f4c262be912d1c8f09649fb2566862bb76e05e8
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-07-22 21:09:32 -0400

    Remove shool_domain module

--- src/school_domain/fuzzy_system_var_interest.py
+++
@@ -1,6 +0,0 @@
-from random import random
-
-class MockInterest:
-    @staticmethod
-    def get_variable_interest() -> float:
-        return random()
--- src/school_domain/professor.py
+++
@@ -1,3 +0,0 @@
-class Professor:
-    def __init__(self):
-        pass
