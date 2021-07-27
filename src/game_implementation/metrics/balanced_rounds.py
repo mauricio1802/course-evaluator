7d1d2706a1cb36f06b0ed7f8089cf3aa715431f9
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-07-26 20:58:33 -0400

    fix: Fix balanced_rounds_metric

--- src/game_implementation/metrics/balanced_rounds.py
+++ src/game_implementation/metrics/balanced_rounds.py
@@ -1,16 +1,16 @@
-from ..utils import round_iterator, rounds_window, get_round_number, total_achievable_points_in_round, total_achievable_points_in_state
+from ..utils import round_iterator, rounds_window, get_round_number, total_achievable_points_in_round
 
 
 def is_balance(round_to_check, rounds_to_check_against):
     points_in_round_to_check = total_achievable_points_in_round(round_to_check)
     points_in_rest_rounds = sum(
-        map(lambda round: points_in_round_to_check(round), rounds_to_check_against))
+        map(lambda round: total_achievable_points_in_round(round), rounds_to_check_against))
     return points_in_round_to_check < points_in_rest_rounds
 
 
 def balanced_rounds_metric(states):
     unbalance_rounds = set()
-    for round_set in rounds_window(round_iterator(states), 4):
+    for round_set in rounds_window(round_iterator(states), 3):
         first_round = round_set[0]
         if not is_balance(first_round, round_set[1:]):
             unbalance_rounds.add(get_round_number(first_round))
