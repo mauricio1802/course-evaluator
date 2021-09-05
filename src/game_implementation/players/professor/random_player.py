e4cadeb395aba8e4686a7fe628463e361fbeb7a7
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-05 14:57:51 -0400

    feat: Create random professor player

---
+++ src/game_implementation/players/professor/random_player.py
@@ -0,0 +1,73 @@
+from typing import List
+from random import random, choice, randint
+from ....game import Play
+from ...state import ChallengeDefinition, Challenge, Professor, GameState
+from ...game import AddChallengePayload, RemoveChallengePayload
+from ....game import Player
+from ...utils import challenge_repr
+
+class ProfessorRandomPlayer(Professor, Player):
+    def __init__(self, id):
+        Professor.__init__(self, id)
+        self._last_challenge_id = 0
+    
+    def set_id(self, id):
+        self.player_id = id
+    
+    def _get_challenge_id(self):
+        self._last_challenge_id += 1
+        return self._last_challenge_id
+    
+    # def play(self, states):
+    #     print("Professor turn")
+    #     play_type = int(input(f"""Select play: 
+    #     1 - End turn
+    #     2 - Add challenge
+    #     3 - Remove challenge\n"""))
+    #     if play_type == 1:
+    #         return Play("finish_turn", None)
+    #     elif play_type == 2:
+    #         interest_required = float(input("Interest required: "))
+    #         cost = int(input("Insert cost: ")) 
+    #         reward = int(input("Insert reward: "))
+    #         student_id = int(input("Student allowed to solve: "))
+    #         challenge_id = self._get_challenge_id()
+    #         challenge_definition = ChallengeDefinition(interest_required, cost, reward)
+    #         challenge = Challenge(challenge_id, challenge_definition, student_id)
+    #         payload = AddChallengePayload(challenge)
+    #         return Play("set_challenge", payload)
+    #     elif play_type == 3:
+    #         challenges_str = "\n".join(map(challenge_repr, states[-1].challenges))
+    #         print(f"""Challenges: 
+    #         {challenges_str}""")
+    #         id_to_remove = int(input("Challenge to remove: "))
+    #         payload = RemoveChallengePayload(challenge_id=id_to_remove)
+    #         return Play("remove_challenge", payload)
+
+    def play(self, states: List[GameState]):
+        play_type = random()
+        if play_type <= 0.20:
+            return Play("finish_turn", None)
+
+        last_state = states[-1] 
+        challenges = last_state.challenges
+
+        if play_type < 0.6 and len(challenges) > 0:
+            challenge_to_remove = choice(challenges)
+            payload = RemoveChallengePayload(challenge_id=challenge_to_remove.get_id())
+            return Play("remove_challenge", payload)
+        
+        players = last_state.players
+        score = randint(5000, 20000)
+        cost = randint(30, 180)
+        interest_required = random()
+        challenge_id = self._get_challenge_id()
+        player_allowed = choice(players).get_id()
+
+        challenge_definition = ChallengeDefinition(interest_required, cost, score)
+        challenge = Challenge(challenge_id, challenge_definition, player_allowed)
+        payload = AddChallengePayload(challenge)
+        return Play("set_challenge", payload)
+
+    def get_id(self):
+        return self.player_id
