c1408c6a125044150e95be135823b091085f61ac
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-09-13 19:45:00 -0400

    feat: Add package for fuzzy systems

---
+++ src/game_implementation/fuzzy_system/__init__.py
@@ -0,0 +1,4 @@
+from .linguistic_var import LinguisticVar
+from .membership_functions import create_bell, create_s, create_gamma, create_triangular
+from .inference_system import FuzzySystem, LARSEN
+from .defuzzification import coa, mom, som, boa
